de6591b6e18e8a4d81aeb4af4072dcdcf6718591
cfbz <crfuenteal2018@udec.cl>
2025-04-08 02:09:29 -0400

    Test 4: codigo minimo para que pase. SUCESS

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,7 +1,15 @@
 def convert(number):
     if number == 4:
         return "IV"
+    
     result = ""
+    
+    if number >= 5:
+        result += "V"
+        number -= 5
+
+
+    
     while number >= 1:
         result += "I"   
         number -= 1 
