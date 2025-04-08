6aa642b389a500e9a6a15fece34b33c907acfeae
cfbz <crfuenteal2018@udec.cl>
2025-04-08 04:19:37 -0400

    Test 9: Refactor para simbolos while en vez de if. SUCESS

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -2,19 +2,19 @@ def convert(number):
  
     result = ""
 
-    if number >= 10:
+    while number >= 10:
         result += "X"
         number -= 10
 
-    if number == 9:
+    while number == 9:
         result+= "IX"
         number -= 9
 
-    if number >= 5:
+    while number >= 5:
         result += "V"
         number -= 5
 
-    if number == 4:
+    while number == 4:
         result+= "IV"
         number -= 4
 
